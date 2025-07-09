d0516262f725fcb3417984e225cb4ac91d78fb77
israr-ax <israrmemon211@gmail.com>
2025-07-09 16:50:58 +0500

    Initial Commit

--- Base/models.py
+++ Base/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     name= models.CharField(max_length=40)
     email=models.EmailField(max_length=40)
     content=models.TextField(max_length=400)
-    number=models.CharField(max_length=10)
+    number=models.CharField(max_length=12)
 
     def __self__(self):
         return(self.name)
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['https://my-portfolio-production-8a54.up.railway.app']
 
 # Application definition
 
